4262874adeaf6932097e14410c536c0ad4193d79
Nikola <nikola.lazarov@smart-soft.net>
2023-03-13 22:57:57 +0200

    Added Extensions method that will be used to configure the database

---
+++ USP-Project.Data/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace USP_Project.Data.Extensions;
+
+public static class DatabaseExtensions
+{
+    public static void SetupDatabase([NotNull] this IServiceCollection services, string connectionString)
+    {
+        if (services is null) throw new ArgumentNullException(nameof(services));
+        services.AddDbContext<DbContext, UspDbContext>(options => options.UseNpgsql(connectionString));
+    }
+}
