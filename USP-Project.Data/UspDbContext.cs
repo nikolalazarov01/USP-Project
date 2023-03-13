b944c1d532d9e792edbb08f72c6cd272608ad70c
Nikola <nikola.lazarov@smart-soft.net>
2023-03-13 22:58:10 +0200

    Added the UspDbContext

---
+++ USP-Project.Data/UspDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace USP_Project.Data;
+
+public class UspDbContext : DbContext
+{
+    public UspDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
