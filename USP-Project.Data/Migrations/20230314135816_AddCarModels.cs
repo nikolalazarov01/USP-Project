55f4e713e68ee8ffa2152398ff68b6183a316d8f
Nikola <nikola.lazarov@smart-soft.net>
2023-03-14 15:58:51 +0200

    Created migration and updated the database

---
+++ USP-Project.Data/Migrations/20230314135816_AddCarModels.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace USP_Project.Data.Migrations
+{
+    public partial class AddCarModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Brand",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brand", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Extra",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Extra", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Model",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Model", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Car",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    BrandId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModelId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Car_Brand_BrandId",
+                        column: x => x.BrandId,
+                        principalTable: "Brand",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_Car_Model_ModelId",
+                        column: x => x.ModelId,
+                        principalTable: "Model",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarExtra",
+                columns: table => new
+                {
+                    CarsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ExtrasId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarExtra", x => new { x.CarsId, x.ExtrasId });
+                    table.ForeignKey(
+                        name: "FK_CarExtra_Car_CarsId",
+                        column: x => x.CarsId,
+                        principalTable: "Car",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CarExtra_Extra_ExtrasId",
+                        column: x => x.ExtrasId,
+                        principalTable: "Extra",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarsExtras",
+                columns: table => new
+                {
+                    CarId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ExtraId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarsExtras", x => new { x.CarId, x.ExtraId });
+                    table.ForeignKey(
+                        name: "FK_CarsExtras_Car_CarId",
+                        column: x => x.CarId,
+                        principalTable: "Car",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CarsExtras_Extra_ExtraId",
+                        column: x => x.ExtraId,
+                        principalTable: "Extra",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Car_BrandId",
+                table: "Car",
+                column: "BrandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Car_ModelId",
+                table: "Car",
+                column: "ModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarExtra_ExtrasId",
+                table: "CarExtra",
+                column: "ExtrasId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarsExtras_ExtraId",
+                table: "CarsExtras",
+                column: "ExtraId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarExtra");
+
+            migrationBuilder.DropTable(
+                name: "CarsExtras");
+
+            migrationBuilder.DropTable(
+                name: "Car");
+
+            migrationBuilder.DropTable(
+                name: "Extra");
+
+            migrationBuilder.DropTable(
+                name: "Brand");
+
+            migrationBuilder.DropTable(
+                name: "Model");
+        }
+    }
+}
