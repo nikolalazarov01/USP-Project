35790861fcace79a424b0525dccd96c1522330a4
Nikola <nikola.lazarov@smart-soft.net>
2023-03-14 15:31:43 +0200

    Configured the relationships in the car model

--- USP-Project.Data/Models/Car.cs
+++ USP-Project.Data/Models/Car.cs
@@ -1,6 +1,22 @@
-﻿namespace USP_Project.Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace USP_Project.Data.Models;
 
 public class Car
 {
+    [Key]
+    public Guid Id { get; set; }
     
+    [Required]
+    [ForeignKey("Brand")]
+    public Guid BrandId { get; set; }
+    public Brand Brand { get; set; }
+    
+    [Required]
+    [ForeignKey("Model")]
+    public Guid ModelId { get; set; }
+    public Model Model { get; set; }
+
+    private ICollection<Extra>? Extras { get; set; }
 }
