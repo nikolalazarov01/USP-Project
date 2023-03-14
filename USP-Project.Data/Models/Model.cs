b684e13f09c28514c9672c62225c07b12e672bd2
Nikola <nikola.lazarov@smart-soft.net>
2023-03-14 15:34:59 +0200

    Created the car model Model and configured the relationship with Car

--- USP-Project.Data/Models/Model.cs
+++ USP-Project.Data/Models/Model.cs
@@ -1,6 +1,14 @@
-﻿namespace USP_Project.Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace USP_Project.Data.Models;
 
 public class Model
 {
+    [Key]
+    public Guid Id { get; set; }
     
+    [Required]
+    public string Name { get; set; }
+    
+    public ICollection<Car> Cars { get; set; }
 }
