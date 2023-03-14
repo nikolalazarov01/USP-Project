9115774d788e3f761b779b5f28f8ddaacafbf0c5
Nikola <nikola.lazarov@smart-soft.net>
2023-03-14 15:37:42 +0200

    Added the many-to-many table for the car and extras

---
+++ USP-Project.Data/Models/CarsExtras.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace USP_Project.Data.Models;
+
+public class CarsExtras
+{
+    [ForeignKey("Car")]
+    public Guid CarId { get; set; }
+    public Car Car { get; set; }
+    
+    [ForeignKey("Extra")]
+    public Guid ExtraId { get; set; }
+    public Extra Extra { get; set; }
+}
