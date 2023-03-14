543e99f73a8ea4b9e5cfd3d87b5a8e21bedd4784
Nikola <nikola.lazarov@smart-soft.net>
2023-03-14 15:36:22 +0200

    Created the car Extra Model and configured the relationships with the Car model

--- USP-Project.Data/Models/Extra.cs
+++ USP-Project.Data/Models/Extra.cs
@@ -1,6 +1,15 @@
-﻿namespace USP_Project.Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace USP_Project.Data.Models;
 
 public class Extra
 {
+    [Key]
+    public Guid Id { get; set; }
     
+    [Required]
+    public string Name { get; set; }
+    
+    public ICollection<Car> Cars { get; set; }
 }
