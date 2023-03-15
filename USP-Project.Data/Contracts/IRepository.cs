49948bb8c207e45c66f07e89f06a57de802d6d1d
Nikola <nikola.lazarov@smart-soft.net>
2023-03-15 19:38:59 +0200

    Created the interface and class for the Repository

---
+++ USP-Project.Data/Contracts/IRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace USP_Project.Data.Contracts;
+
+public interface IRepository<T> where T : class
+{
+    
+}
---
+++ USP-Project.Data/Repository/Repository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using USP_Project.Data.Contracts;
+
+namespace USP_Project.Data.Repository;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly UspDbContext _db;
+    protected readonly DbSet<T> _dbSet;
+
+    public Repository(UspDbContext db)
+    {
+        _db = db;
+        _dbSet = _db.Set<T>();
+    }
+}
