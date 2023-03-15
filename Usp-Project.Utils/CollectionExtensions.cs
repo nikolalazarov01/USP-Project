0a5220a74b740a3e45470f86450415f40d250b1d
Nikola <nikola.lazarov@smart-soft.net>
2023-03-15 20:26:21 +0200

    Added new CollectionExtensions class that has methods to filter the null values

---
+++ Usp-Project.Utils/CollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Usp_Project.Utils;
+
+public static class CollectionExtensions
+{
+    public static IEnumerable<T> OrEmptyIfNull<T>(this IEnumerable<T> collection)
+    {
+        return collection ?? Enumerable.Empty<T>();
+    }
+
+    public static IEnumerable<T> IgnoreNullValues<T>(this IEnumerable<T> collection)
+        where T : class
+    {
+        if (collection is null) throw new ArgumentNullException(nameof(collection));
+
+        return collection.Where(el => el is not null);
+    }
+}
