4f5b68c0812d2216028c356f0b39cc4c859a7238
Nikola <nikola.lazarov@smart-soft.net>
2023-03-15 19:52:55 +0200

    Added new validation method in the operation result extensions

---
+++ Usp-Project.Utils/OperationResultExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Usp_Project.Utils;
+
+public static class OperationResultExtensions
+{
+    public static bool ValidateNotNull<TValue>(this OperationResult operationResult,
+        [NotNullWhen(true)] TValue? value, [CallerFilePath] string? filePath = null,
+        [CallerMemberName] string? memberName = null, [CallerLineNumber] int line = -1,
+        [CallerArgumentExpression("value")] string? expression = null)
+    {
+        if (operationResult is null) throw new ArgumentNullException(nameof(operationResult));
+        if (value is not null) return true;
+
+        var error = $"{FormatErrorMessage(filePath, memberName, line, expression)} should not be null.";
+        operationResult.AddError(error);
+
+        return false;
+    }
+    
+    private static string FormatErrorMessage(string filePath, string memberName, int line, string argumentExpression) => $"{filePath} ({memberName};{line}) - Expression [{argumentExpression}]";
+}
