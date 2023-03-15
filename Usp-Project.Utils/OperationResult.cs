da7a18033bf4e3fad7f28362ee1b9791b5a697f8
Nikola <nikola.lazarov@smart-soft.net>
2023-03-15 19:44:23 +0200

    Added new 'Utils' project and new class 'OperationResult'

---
+++ Usp-Project.Utils/OperationResult.cs
@@ -0,0 +1,24 @@
+﻿namespace Usp_Project.Utils;
+
+public class OperationResult
+{
+    private List<string> _errors = new();
+    public IReadOnlyCollection<string> Errors => this._errors.AsReadOnly();
+
+    public bool IsSuccessfull => !this._errors.Any();
+
+    public bool AddError(string errorMessage)
+    {
+        if (errorMessage is null) return false;
+        
+        this._errors.Add(errorMessage);
+        return true;
+    }
+
+    public override string ToString() => string.Join(Environment.NewLine, this._errors);
+}
+
+public class OperationResult<T> : OperationResult where T : class
+{
+    public T? Data { get; set; }
+}
