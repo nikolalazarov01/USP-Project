8ab8dd8d21bc8a8ab78db74458e51bdd67fc4f49
Victorio Nikolaev <vikinik01@abv.bg>
2023-03-23 00:39:26 +0200

    feat(controller): Adding a Controller and a Model
    Adding a Cars Controller and a Create Car Input Model.

---
+++ USP-Project.Web/Controllers/CarsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using USP_Project.Core.Contracts;
+using USP_Project.Web.Models.Cars;
+
+namespace USP_Project.Web.Controllers;
+
+public class CarsController : Controller
+{
+    private readonly ICarsService _carsService;
+    
+    private const string ErrorsKey = "Errors";
+
+    public CarsController(ICarsService carsService)
+        => _carsService = carsService;
+
+    // TODO: Create a view with a form for creating a new Car:
+    [HttpGet]
+    public IActionResult Add() => Ok();
+        // => View(new CreateCarInputModel());
+
+    [HttpPost]
+    public async Task<IActionResult> Add([FromForm] CreateCarInputModel carInputModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            ViewData[ErrorsKey] = ModelState
+                .SelectMany(x =>
+                    x.Value?.Errors
+                        .Select(e => e.ErrorMessage)
+                                 ?? Array.Empty<string>());
+            return RedirectToAction(nameof(Add));
+        }
+        
+        var result = await _carsService.CreateAsync(
+            carInputModel.BrandName,
+            carInputModel.BrandDescription,
+            carInputModel.ModelName,
+            carInputModel.Extras);
+
+        if (!result.IsSuccessfull)
+        {
+            ViewData[ErrorsKey] = result.Errors;
+            return RedirectToAction(nameof(Add));
+        }
+
+        // TODO: Potentially add a Success message to the View Data ...
+        return RedirectToAction(nameof(HomeController.Index), "Home");
+    }
+}
