8e570d84d1c3bc095852b83141df1472befa5778
Nikola <nikola.lazarov@smart-soft.net>
2023-04-01 11:41:06 +0300

    Added IFileService interface and FileService service

---
+++ USP-Project.Core/Contracts/IFileService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Usp_Project.Utils;
+
+namespace USP_Project.Core.Contracts;
+
+public interface IFileService
+{
+    Task<OperationResult> Upload(IFormFile fileToUpload, string filePath);
+}
---
+++ USP-Project.Core/Services/FileService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using USP_Project.Core.Contracts;
+using Usp_Project.Utils;
+
+namespace USP_Project.Core.Services;
+
+public class FileService : IFileService
+{
+    public async Task<OperationResult> Upload(IFormFile? fileToUpload, string filePath)
+    {
+        var operationResult = new OperationResult();
+        
+        if (fileToUpload is null)
+        {
+            operationResult.AddError("The provided file is null!");
+            return operationResult;
+        }
+
+        if (!Directory.Exists(filePath))
+
+            Directory.CreateDirectory(filePath);
+
+        var fileName = fileToUpload.FileName;
+
+        var fullPath = Path.Combine(filePath, fileName);
+
+
+        await using FileStream fs = System.IO.File.Create(fullPath);
+        await fileToUpload.CopyToAsync(fs);
+
+        return operationResult;
+    }
+}
